125bbc746a2dbd61eba6e8689985408f174e5a60
Sir Iyanu <iyolawoye@gmail.com>
2022-06-22 09:50:35 +0100

    1st Commit

---
+++ I4G013798EYK/blog/views.py
@@ -0,0 +1,30 @@
+from re import template
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields =    "__all__"
+    success_url: reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url: reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url: reverse_lazy("blog:all")
+
